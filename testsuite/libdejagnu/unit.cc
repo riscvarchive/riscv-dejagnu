97ce8881329877b6b2e87bbf4d19e4ad5acbd435
Ben Elliston <bje@gnu.org>
2006-01-01 07:00:11 +0000

    * config/default.exp, config/unix.exp, config/vxworks.exp, 	lib/debugger.exp, lib/dejagnu.exp, lib/framework.exp, 	lib/libgloss.exp, lib/remote.exp, testsuite/lib/util-defs.exp: 	Formatting fixes. 	* dejagnu.h, testsuite/config/default.exp, 	testsuite/lib/libsup.exp, testsuite/libdejagnu/tunit.exp, 	testsuite/libdejagnu/unit.cc, testsuite/runtest.all/libs.exp, 	testsuite/runtest.all/options.exp, 	testsuite/runtest.all/stats-sub.exp, 	testsuite/runtest.all/stats.exp: Update copyright notice and FSF 	office address. Remove bug reporting address, as it is now 	maintained centrally in the README file.

--- testsuite/libdejagnu/unit.cc
+++ testsuite/libdejagnu/unit.cc
@@ -12,8 +12,8 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// along with DejaGnu; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.
 
 // Please email any bugs, comments, and/or additions to this file to:
 // bug-dejagnu@gnu.org
